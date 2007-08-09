311a15431ec27bb5b28adc1eca40b829a0907d89
Peter Johnson <pete@neo.bilogic.org>
2007-08-09 11:08:18 -0700

    Add OrgBytecode implementation.

---
+++ libyasm/bc-org.cpp
@@ -0,0 +1,162 @@
+///
+/// ORG bytecode
+///
+///  Copyright (C) 2005-2007  Peter Johnson
+///
+/// Redistribution and use in source and binary forms, with or without
+/// modification, are permitted provided that the following conditions
+/// are met:
+/// 1. Redistributions of source code must retain the above copyright
+///    notice, this list of conditions and the following disclaimer.
+/// 2. Redistributions in binary form must reproduce the above copyright
+///    notice, this list of conditions and the following disclaimer in the
+///    documentation and/or other materials provided with the distribution.
+///
+/// THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND OTHER CONTRIBUTORS ``AS IS''
+/// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+/// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+/// ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR OTHER CONTRIBUTORS BE
+/// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+/// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+/// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+/// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+/// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+/// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+/// POSSIBILITY OF SUCH DAMAGE.
+///
+#include "util.h"
+
+#include <iomanip>
+
+#include "bytecode.h"
+#include "errwarn.h"
+#include "file.h"
+
+
+namespace {
+
+using namespace yasm;
+
+class OrgBytecode : public Bytecode::Contents {
+public:
+    OrgBytecode(unsigned long start, unsigned long fill);
+    ~OrgBytecode();
+
+    /// Prints the implementation-specific data (for debugging purposes).
+    void put(std::ostream& os, int indent_level) const;
+
+    /// Finalizes the bytecode after parsing.
+    void finalize(Bytecode& bc, Bytecode& prev_bc);
+
+    /// Calculates the minimum size of a bytecode.
+    unsigned long calc_len(Bytecode& bc, Bytecode::AddSpanFunc add_span);
+
+    /// Recalculates the bytecode's length based on an expanded span
+    /// length.
+    bool expand(Bytecode& bc, unsigned long& len, int span,
+                long old_val, long new_val,
+                /*@out@*/ long& neg_thres,
+                /*@out@*/ long& pos_thres);
+
+    /// Convert a bytecode into its byte representation.
+    void to_bytes(Bytecode& bc, unsigned char* &buf,
+                  OutputValueFunc output_value,
+                  OutputRelocFunc output_reloc = 0);
+
+    SpecialType get_special() const;
+
+    OrgBytecode* clone() const;
+
+private:
+    unsigned long m_start;      ///< target starting offset within section
+    unsigned long m_fill;       ///< fill value
+};
+
+
+OrgBytecode::OrgBytecode(unsigned long start, unsigned long fill)
+    : m_start(start),
+      m_fill(fill)
+{
+}
+
+OrgBytecode::~OrgBytecode()
+{
+}
+
+void
+OrgBytecode::put(std::ostream& os, int indent_level) const
+{
+    os << std::setw(indent_level) << "" << "_Org_" << std::endl;
+    os << std::setw(indent_level) << "" << "Start=" << m_start << std::endl;
+    os << std::setw(indent_level) << "" << "Fill=" << m_fill << std::endl;
+}
+
+void
+OrgBytecode::finalize(Bytecode& bc, Bytecode& prev_bc)
+{
+}
+
+unsigned long
+OrgBytecode::calc_len(Bytecode& bc, Bytecode::AddSpanFunc add_span)
+{
+    unsigned long len = 0;
+    long neg_thres = 0;
+    long pos_thres = m_start;
+
+    expand(bc, len, 0, 0, (long)bc.get_offset(), neg_thres, pos_thres);
+
+    return len;
+}
+
+bool
+OrgBytecode::expand(Bytecode& bc, unsigned long& len, int span,
+                    long old_val, long new_val,
+                    /*@out@*/ long& neg_thres,
+                    /*@out@*/ long& pos_thres)
+{
+    // Check for overrun
+    if ((unsigned long)new_val > m_start)
+        throw Error(N_("ORG overlap with already existing data"));
+
+    // Generate space to start offset
+    len = m_start - new_val;
+    return true;
+}
+
+void
+OrgBytecode::to_bytes(Bytecode& bc, unsigned char* &buf,
+                      OutputValueFunc output_value,
+                      OutputRelocFunc output_reloc)
+{
+    // Sanity check for overrun
+    if (bc.get_offset() > m_start)
+        throw Error(N_("ORG overlap with already existing data"));
+    unsigned long len = m_start - bc.get_offset();
+    for (unsigned long i=0; i<len; i++)
+        WRITE_8(buf, m_fill);       // XXX: handle more than 8 bit?
+}
+
+OrgBytecode::SpecialType
+OrgBytecode::get_special() const
+{
+    return SPECIAL_OFFSET;
+}
+
+OrgBytecode*
+OrgBytecode::clone() const
+{
+    return new OrgBytecode(m_start, m_fill);
+}
+
+} // anonymous namespace
+
+namespace yasm {
+
+std::auto_ptr<Bytecode>
+create_org(unsigned long start, unsigned long fill, unsigned long line)
+{
+    Bytecode::Contents::Ptr contents(new OrgBytecode(start, fill));
+    return std::auto_ptr<Bytecode>(new Bytecode(contents, line));
+}
+
+} // namespace yasm
