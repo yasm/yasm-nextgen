5b26739bcfba92d59b115d15eef415f9dd631736
Peter Johnson <pete@neo.bilogic.org>
2007-08-08 17:29:42 -0700

    Add directive helpers.

---
+++ libyasm/dir_help.cpp
@@ -0,0 +1,160 @@
+///
+/// YASM directive helper implementation
+///
+///  Copyright (C) 2001-2007  Peter Johnson
+///
+/// Redistribution and use in source and binary forms, with or without
+/// modification, are permitted provided that the following conditions
+/// are met:
+/// 1. Redistributions of source code must retain the above copyright
+///    notice, this list of conditions and the following disclaimer.
+/// 2. Redistributions in binary form must reproduce the above copyright
+///    notice, this list of conditions and the following disclaimer in the
+///    documentation and/or other materials provided with the distribution.
+///
+/// THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND OTHER CONTRIBUTORS ``AS IS''
+/// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+/// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+/// ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR OTHER CONTRIBUTORS BE
+/// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+/// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+/// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+/// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+/// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+/// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+/// POSSIBILITY OF SUCH DAMAGE.
+///
+#include "util.h"
+
+#include <map>
+
+#include <boost/format.hpp>
+
+#include "dir_help.h"
+#include "errwarn.h"
+#include "expr.h"
+#include "intnum.h"
+#include "object.h"
+
+
+namespace yasm {
+
+class DirHelperManager::Impl {
+public:
+    Impl() {}
+    ~Impl() {}
+
+    typedef std::map<std::string, boost::function<void (const NameValue&)> >
+        HelperMap;
+    HelperMap m_value_helpers, m_novalue_helpers;
+};
+
+DirHelperManager::DirHelperManager()
+    : m_impl(new Impl)
+{
+}
+
+DirHelperManager::~DirHelperManager()
+{
+}
+
+void
+DirHelperManager::add(const std::string& name, bool needsvalue,
+                      boost::function<void (const NameValue&)> helper)
+{
+    if (needsvalue)
+        m_impl->m_value_helpers.insert(std::make_pair(name, helper));
+    else
+        m_impl->m_novalue_helpers.insert(std::make_pair(name, helper));
+}
+
+bool
+DirHelperManager::operator()
+    (NameValues::const_iterator nv_first,
+     NameValues::const_iterator nv_last,
+     boost::function<bool (const NameValue&)> helper_nameval)
+{
+    bool anymatched = false;
+
+    for (NameValues::const_iterator nv=nv_first; nv != nv_last; ++nv) {
+        bool matched = false;
+
+        if (nv->get_name().empty() && nv->is_id()) {
+            Impl::HelperMap::iterator helper =
+                m_impl->m_novalue_helpers.find(nv->get_id());
+            if (helper != m_impl->m_novalue_helpers.end()) {
+                helper->second(*nv);
+                matched = true;
+                anymatched = true;
+            }
+        } else if (!nv->get_name().empty()) {
+            Impl::HelperMap::iterator helper =
+                m_impl->m_value_helpers.find(nv->get_id());
+            if (helper != m_impl->m_novalue_helpers.end()) {
+                helper->second(*nv);
+                matched = true;
+                anymatched = true;
+            }
+        }
+
+        if (!matched) {
+            if (helper_nameval(*nv))
+                anymatched = true;
+        }
+    }
+
+    return anymatched;
+}
+
+void
+dir_intn(const NameValue& nv, Object& obj, unsigned long line, IntNum& out,
+         bool& out_set)
+{
+    std::auto_ptr<Expr> e(nv.get_expr(obj, line));
+    /*@null@*/ IntNum* local;
+
+    if (!e.get() || !(local = e->get_intnum())) {
+        throw NotConstantError(str(boost::format(
+            N_("argument to `%s' is not an integer")) % nv.get_name()));
+    }
+
+    out = *local;
+    out_set = true;
+}
+
+void
+dir_string(const NameValue& nv, std::string& out, bool& out_set)
+{
+    if (!nv.is_string()) {
+        throw ValueError(str(boost::format(
+            N_("argument to `%s' is not a string or identifier")) %
+                             nv.get_name()));
+    }
+    out = nv.get_string();
+    out_set = true;
+}
+
+bool
+dir_nameval_warn(const NameValue& nv)
+{
+    if (!nv.get_name().empty()) {
+        warn_set(WARN_GENERAL,
+                 str(boost::format(N_("Unrecognized qualifier `%s'")) %
+                     nv.get_name()));
+        return false;
+    }
+
+    if (nv.is_id()) {
+        warn_set(WARN_GENERAL,
+                 str(boost::format(N_("Unrecognized qualifier `%s'")) %
+                     nv.get_id()));
+    } else if (nv.is_string()) {
+        warn_set(WARN_GENERAL, N_("Unrecognized string qualifier"));
+    } else {
+        warn_set(WARN_GENERAL, N_("Unrecognized numeric qualifier"));
+    }
+
+    return false;
+}
+
+} // namespace yasm
