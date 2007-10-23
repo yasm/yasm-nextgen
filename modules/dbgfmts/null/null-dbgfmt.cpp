446f20600a7e3c87f51a53a4e8b75138e08dead4
Peter Johnson <peter@tortall.net>
2007-10-22 22:34:08 -0700

    Add null debug format.

---
+++ modules/dbgfmts/null/null-dbgfmt.cpp
@@ -0,0 +1,50 @@
+//
+// Null debugging format (creates NO debugging information)
+//
+//  Copyright (C) 2002-2007  Peter Johnson
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND OTHER CONTRIBUTORS ``AS IS''
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR OTHER CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+#include <util.h>
+
+#include <libyasm/debug_format.h>
+#include <libyasm/factory.h>
+
+
+namespace yasm { namespace dbgfmt { namespace null {
+
+class NullDebug : public DebugFormat {
+public:
+    NullDebug() {}
+    ~NullDebug() {}
+
+    std::string get_name() const { return "No debugging info"; }
+    std::string get_keyword() const { return "null"; }
+
+    bool set_object(Object* object) { return true; }
+    void generate(Linemap& linemap, Errwarns& errwarns) {}
+};
+
+ddj::registerInFactory<DebugFormat, NullDebug> registerNullDebug("null");
+bool static_ref = true;
+
+}}} // namespace yasm::dbgfmt::null
