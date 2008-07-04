yasm/yasm-nextgen

commit 4ecda7b226173d67b6f405d6af907e148d58c43e

Bytecode::Fixup::swap(): Add. Add ability to add fixup for a std::auto_ptr<Value>. Use swaps to reduce Fixup construction copying.

libyasmx/bytecode.cpp
@@ -280,20 +280,51 @@ Bytecode::update_offset(unsigned long offset)
 void
 Bytecode::append_fixed(const Value& val)
 {
-    m_fixed_fixups.push_back(Fixup(m_fixed.size(), val, m_line));
     m_fixed.write(val.m_size/8, 0);
+    m_fixed_fixups.push_back(Fixup(m_fixed.size(), val, m_line));
+}
+
+void
+Bytecode::append_fixed(std::auto_ptr<Value> val)
+{
+    m_fixed.write(val->m_size/8, 0);
+    m_fixed_fixups.push_back(Fixup(m_fixed.size(), val, m_line));
 }
 
 void
 Bytecode::append_fixed(unsigned int size, std::auto_ptr<Expr> e)
 {
-    m_fixed_fixups.push_back(Fixup(m_fixed.size(), Value(size*8, e), m_line));
     m_fixed.write(size, 0);
+    m_fixed_fixups.push_back(Fixup(m_fixed.size(), size*8, e, m_line));
 }
 
 Bytecode::Fixup::Fixup(unsigned int off, const Value& val, unsigned long line)
     : Value(val), m_line(line), m_off(off)
 {
+}
+
+Bytecode::Fixup::Fixup(unsigned int off,
+                       std::auto_ptr<Value> val,
+                       unsigned long line)
+    : Value(0), m_line(line), m_off(off)
+{
+    Value::swap(*val);
+}
+
+Bytecode::Fixup::Fixup(unsigned int off,
+                       unsigned int size,
+                       std::auto_ptr<Expr> e,
+                       unsigned long line)
+    : Value(size, e), m_line(line), m_off(off)
+{
+}
+
+void
+Bytecode::Fixup::swap(Fixup& oth)
+{
+    Value::swap(oth);
+    std::swap(m_line, oth.m_line);
+    std::swap(m_off, oth.m_off);
 }
 
 } // namespace yasm

libyasmx/bytecode.h
@@ -399,6 +399,7 @@ public:
     const Bytes& get_fixed() const { return m_fixed; }
 
     void append_fixed(const Value& val);
+    void append_fixed(std::auto_ptr<Value> val);
     void append_fixed(unsigned int size, std::auto_ptr<Expr> e);
 private:
     /// Fixed data that comes before the possibly dynamic length data generated
@@ -412,12 +413,19 @@ private:
     {
     public:
         Fixup(unsigned int off, const Value& val, unsigned long line);
+        Fixup(unsigned int off, std::auto_ptr<Value> val, unsigned long line);
+        Fixup(unsigned int off,
+              unsigned int size,
+              std::auto_ptr<Expr> e,
+              unsigned long line);
+        void swap(Fixup& oth);
         unsigned long get_line() const { return m_line; }
         unsigned int get_off() const { return m_off; }
     private:
         unsigned long m_line;
         unsigned int m_off;
     };
+    friend void swap(Fixup& left, Fixup& right);
 
     std::vector<Fixup> m_fixed_fixups;
 
@@ -453,6 +461,12 @@ operator<< (marg_ostream& os, const Bytecode::Contents& contents)
 
 YASM_LIB_EXPORT
 marg_ostream& operator<< (marg_ostream &os, const Bytecode& bc);
+
+inline void
+swap(Bytecode::Fixup& left, Bytecode::Fixup& right)
+{
+    left.swap(right);
+}
 
 } // namespace yasm
 

commit be0ebd11bbc544c2df71ec30f41bc0c7faa3368b

Fix breakage introduced in 4ecda7b226173d67b6f405d6af907e148d58c43e. We use m_fixed.size() when constructing the fixup, so we have to be sure to write the placeholder 0s *after* we have read the size.

libyasmx/bytecode.cpp
@@ -280,22 +280,24 @@ Bytecode::update_offset(unsigned long offset)
 void
 Bytecode::append_fixed(const Value& val)
 {
-    m_fixed.write(val.m_size/8, 0);
+    unsigned int valsize = val.m_size/8;
     m_fixed_fixups.push_back(Fixup(m_fixed.size(), val, m_line));
+    m_fixed.write(valsize, 0);
 }
 
 void
 Bytecode::append_fixed(std::auto_ptr<Value> val)
 {
-    m_fixed.write(val->m_size/8, 0);
+    unsigned int valsize = val->m_size/8;
     m_fixed_fixups.push_back(Fixup(m_fixed.size(), val, m_line));
+    m_fixed.write(valsize, 0);
 }
 
 void
 Bytecode::append_fixed(unsigned int size, std::auto_ptr<Expr> e)
 {
-    m_fixed.write(size, 0);
     m_fixed_fixups.push_back(Fixup(m_fixed.size(), size*8, e, m_line));
+    m_fixed.write(size, 0);
 }
 
 Bytecode::Fixup::Fixup(unsigned int off, const Value& val, unsigned long line)