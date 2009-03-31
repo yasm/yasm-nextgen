96e074c255b0dfdb2748577b9c2743104aea87e7
Peter Johnson <peter@tortall.net>
2009-03-30 23:14:31 -0700

    Rename leb128.cpp/h to Bytes_leb128.cpp/h.

--- libyasmx/Bytes_leb128.cpp
+++ libyasmx/Bytes_leb128.cpp
@@ -24,7 +24,7 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 //
-#include "leb128.h"
+#include "Bytes_leb128.h"
 
 #include "Bytes.h"
 #include "IntNum.h"
