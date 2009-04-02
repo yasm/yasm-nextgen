37122896e4a07399013cdd8ccebc5de5b83c1e2f
Peter Johnson <peter@tortall.net>
2009-04-01 20:34:10 -0700

    YasmCXXTR1.cmake: Fix for standards compliance.
    Newer compilers (G++ 4.3) don't let you take the address of main, so the
    std::function checks were failing.
    
    Also check for std:: and std::tr1 versions in both functional and
    tr1/functional headers just in case.

--- cmake/modules/YasmCXXTR1.cmake
+++ cmake/modules/YasmCXXTR1.cmake
@@ -55,55 +55,39 @@ CHECK_INCLUDE_FILE_CXX(tr1/functional HAVE_TR1_FUNCTIONAL)
 
 # function
 IF(HAVE_TR1_FUNCTIONAL)
-    YASM_CHECK_CXX("std::tr1::function<int ()> x = main;" "std::tr1::function" "tr1/functional" HAVE_TR1_FUNCTION)
+    YASM_CHECK_CXX("std::function<void (int)> x = f2;" "std::function" "tr1/functional" HAVE_STD_FUNCTION1)
+    YASM_CHECK_CXX("std::tr1::function<void (int)> x = f2;" "std::tr1::function" "tr1/functional" HAVE_TR1_FUNCTION1)
 ENDIF(HAVE_TR1_FUNCTIONAL)
 
-IF(NOT HAVE_TR1_FUNCTION)
-    YASM_CHECK_CXX("std::tr1::function<int ()> x = main;" "std::tr1::function" "functional" HAVE_TR1_FUNCTION)
-ENDIF(NOT HAVE_TR1_FUNCTION)
-
-IF(NOT HAVE_TR1_FUNCTION)
-    YASM_CHECK_CXX("std::function<int ()> x = main;" "std::function" "functional" HAVE_STD_FUNCTION)
-ENDIF(NOT HAVE_TR1_FUNCTION)
+YASM_CHECK_CXX("std::function<void (int)> x = f2;" "std::function" "functional" HAVE_STD_FUNCTION2)
+YASM_CHECK_CXX("std::tr1::function<void (int)> x = f2;" "std::tr1::function" "functional" HAVE_TR1_FUNCTION2)
 
 # ref
 IF(HAVE_TR1_FUNCTIONAL)
-    YASM_CHECK_CXX("X x1;\n  X& x2 = std::tr1::ref(x1);" "std::tr1::ref" "tr1/functional" HAVE_TR1_REF)
+    YASM_CHECK_CXX("X x1;\n  X& x2 = std::ref(x1);" "std::ref" "tr1/functional" HAVE_STD_REF1)
+    YASM_CHECK_CXX("X x1;\n  X& x2 = std::tr1::ref(x1);" "std::tr1::ref" "tr1/functional" HAVE_TR1_REF1)
 ENDIF(HAVE_TR1_FUNCTIONAL)
 
-IF(NOT HAVE_TR1_REF)
-    YASM_CHECK_CXX("X x1;\n  X& x2 = std::tr1::ref(x1);" "std::tr1::ref" "functional" HAVE_TR1_REF)
-ENDIF(NOT HAVE_TR1_REF)
-
-IF(NOT HAVE_TR1_REF)
-    YASM_CHECK_CXX("X x1;\n  X& x2 = std::ref(x1);" "std::ref" "functional" HAVE_STD_REF)
-ENDIF(NOT HAVE_TR1_REF)
+YASM_CHECK_CXX("X x1;\n  X& x2 = std::ref(x1);" "std::ref" "functional" HAVE_STD_REF2)
+YASM_CHECK_CXX("X x1;\n  X& x2 = std::tr1::ref(x1);" "std::tr1::ref" "functional" HAVE_TR1_REF2)
 
 # bind
 IF(HAVE_TR1_FUNCTIONAL)
-    YASM_CHECK_CXX("std::tr1::bind(f2, 1)();" "std::tr1::bind" "tr1/functional" HAVE_TR1_BIND)
+    YASM_CHECK_CXX("std::bind(f2, 1)();" "std::bind" "tr1/functional" HAVE_STD_BIND1)
+    YASM_CHECK_CXX("std::tr1::bind(f2, 1)();" "std::tr1::bind" "tr1/functional" HAVE_TR1_BIND1)
 ENDIF(HAVE_TR1_FUNCTIONAL)
 
-IF(NOT HAVE_TR1_BIND)
-    YASM_CHECK_CXX("std::tr1::bind(f2, 1)();" "std::tr1::bind" "functional" HAVE_TR1_BIND)
-ENDIF(NOT HAVE_TR1_BIND)
-
-IF(NOT HAVE_TR1_BIND)
-    YASM_CHECK_CXX("std::bind(f2, 1)();" "std::bind" "functional" HAVE_STD_BIND)
-ENDIF(NOT HAVE_TR1_BIND)
+YASM_CHECK_CXX("std::bind(f2, 1)();" "std::bind" "functional" HAVE_STD_BIND2)
+YASM_CHECK_CXX("std::tr1::bind(f2, 1)();" "std::tr1::bind" "functional" HAVE_TR1_BIND2)
 
 # mem_fn
 IF(HAVE_TR1_FUNCTIONAL)
-    YASM_CHECK_CXX("std::vector<X> v;\n  std::for_each(v.begin(), v.end(), std::tr1::mem_fn(&X::f));" "std::tr1::mem_fn" "tr1/functional" HAVE_TR1_MEMFN)
+    YASM_CHECK_CXX("std::vector<X> v;\n  std::for_each(v.begin(), v.end(), std::mem_fn(&X::f));" "std::mem_fn" "tr1/functional" HAVE_STD_MEMFN1)
+    YASM_CHECK_CXX("std::vector<X> v;\n  std::for_each(v.begin(), v.end(), std::tr1::mem_fn(&X::f));" "std::tr1::mem_fn" "tr1/functional" HAVE_TR1_MEMFN1)
 ENDIF(HAVE_TR1_FUNCTIONAL)
 
-IF(NOT HAVE_TR1_MEMFN)
-    YASM_CHECK_CXX("std::vector<X> v;\n  std::for_each(v.begin(), v.end(), std::tr1::mem_fn(&X::f));" "std::tr1::mem_fn" "functional" HAVE_TR1_MEMFN)
-ENDIF(NOT HAVE_TR1_MEMFN)
-
-IF(NOT HAVE_TR1_MEMFN)
-    YASM_CHECK_CXX("std::vector<X> v;\n  std::for_each(v.begin(), v.end(), std::mem_fn(&X::f));" "std::mem_fn" "functional" HAVE_STD_MEMFN)
-ENDIF(NOT HAVE_TR1_MEMFN)
+YASM_CHECK_CXX("std::vector<X> v;\n  std::for_each(v.begin(), v.end(), std::mem_fn(&X::f));" "std::mem_fn" "functional" HAVE_STD_MEMFN2)
+YASM_CHECK_CXX("std::vector<X> v;\n  std::for_each(v.begin(), v.end(), std::tr1::mem_fn(&X::f));" "std::tr1::mem_fn" "functional" HAVE_TR1_MEMFN2)
 
 #
 # Generate functional.h code
@@ -121,50 +105,50 @@ ENDIF(HAVE_TR1_FUNCTIONAL)
 SET(BOOST_REQUIRED FALSE)
 
 # function
-IF(HAVE_STD_FUNCTION)
+IF(HAVE_STD_FUNCTION1 OR HAVE_STD_FUNCTION2)
     SET(FUNCTIONAL_ALIASES "${FUNCTIONAL_ALIASES}namespace FUNCTION = std;\n")
-ELSEIF(HAVE_TR1_FUNCTION)
+ELSEIF(HAVE_TR1_FUNCTION1 OR HAVE_TR1_FUNCTION2)
     SET(FUNCTIONAL_ALIASES "${FUNCTIONAL_ALIASES}namespace FUNCTION = std::tr1;\n")
-ELSE(HAVE_STD_FUNCTION)
+ELSE(HAVE_STD_FUNCTION1 OR HAVE_STD_FUNCTION2)
     SET(FUNCTIONAL_INCLUDES "${FUNCTIONAL_INCLUDES}#include <boost/function.hpp>\n")
     SET(FUNCTIONAL_ALIASES "${FUNCTIONAL_ALIASES}namespace FUNCTION = boost;\n")
     SET(BOOST_REQUIRED TRUE)
-ENDIF(HAVE_STD_FUNCTION)
+ENDIF(HAVE_STD_FUNCTION1 OR HAVE_STD_FUNCTION2)
 
 # ref
-IF(HAVE_STD_REF)
+IF(HAVE_STD_REF1 OR HAVE_STD_REF2)
     SET(FUNCTIONAL_ALIASES "${FUNCTIONAL_ALIASES}namespace REF = std;\n")
-ELSEIF(HAVE_TR1_REF)
+ELSEIF(HAVE_TR1_REF1 OR HAVE_TR1_REF2)
     SET(FUNCTIONAL_ALIASES "${FUNCTIONAL_ALIASES}namespace REF = std::tr1;\n")
-ELSE(HAVE_STD_FUNCTION)
+ELSE(HAVE_STD_REF1 OR HAVE_STD_REF2)
     SET(FUNCTIONAL_INCLUDES "${FUNCTIONAL_INCLUDES}#include <boost/ref.hpp>\n")
     SET(FUNCTIONAL_ALIASES "${FUNCTIONAL_ALIASES}namespace REF = boost;\n")
     SET(BOOST_REQUIRED TRUE)
-ENDIF(HAVE_STD_REF)
+ENDIF(HAVE_STD_REF1 OR HAVE_STD_REF2)
 
 # bind
-IF(HAVE_STD_BIND)
+IF(HAVE_STD_BIND1 OR HAVE_STD_BIND2)
     SET(FUNCTIONAL_ALIASES "${FUNCTIONAL_ALIASES}namespace BIND = std;\n")
     SET(FUNCTIONAL_ALIASES "${FUNCTIONAL_ALIASES}using namespace std::placeholders;\n")
-ELSEIF(HAVE_TR1_BIND)
+ELSEIF(HAVE_TR1_BIND1 OR HAVE_TR1_BIND2)
     SET(FUNCTIONAL_ALIASES "${FUNCTIONAL_ALIASES}namespace BIND = std::tr1;\n")
     SET(FUNCTIONAL_ALIASES "${FUNCTIONAL_ALIASES}using namespace std::tr1::placeholders;\n")
-ELSE(HAVE_STD_FUNCTION)
+ELSE(HAVE_STD_BIND1 OR HAVE_STD_BIND2)
     SET(FUNCTIONAL_INCLUDES "${FUNCTIONAL_INCLUDES}#include <boost/bind.hpp>\n")
     SET(FUNCTIONAL_ALIASES "${FUNCTIONAL_ALIASES}namespace BIND = boost;\n")
     SET(BOOST_REQUIRED TRUE)
-ENDIF(HAVE_STD_BIND)
+ENDIF(HAVE_STD_BIND1 OR HAVE_STD_BIND2)
 
 # mem_fn
-IF(HAVE_STD_MEMFN)
+IF(HAVE_STD_MEMFN1 OR HAVE_STD_MEMFN2)
     SET(FUNCTIONAL_ALIASES "${FUNCTIONAL_ALIASES}namespace MEMFN = std;\n")
-ELSEIF(HAVE_TR1_MEMFN)
+ELSEIF(HAVE_TR1_MEMFN1 OR HAVE_TR1_MEMFN2)
     SET(FUNCTIONAL_ALIASES "${FUNCTIONAL_ALIASES}namespace MEMFN = std::tr1;\n")
-ELSE(HAVE_STD_MEMFN)
+ELSE(HAVE_STD_MEMFN1 OR HAVE_STD_MEMFN2)
     SET(FUNCTIONAL_INCLUDES "${FUNCTIONAL_INCLUDES}#include <boost/mem_fn.hpp>\n")
     SET(FUNCTIONAL_ALIASES "${FUNCTIONAL_ALIASES}namespace MEMFN = boost;\n")
     SET(BOOST_REQUIRED TRUE)
-ENDIF(HAVE_STD_MEMFN)
+ENDIF(HAVE_STD_MEMFN1 OR HAVE_STD_MEMFN2)
 
 IF(BOOST_REQUIRED AND NOT Boost_FOUND)
   MESSAGE(FATAL_ERROR "Couldn't find the Boost libraries and/or include directory to fulfill TR1 requirements.  Please install Boost or a TR1 development package. You can set BOOST_ROOT, BOOST_INCLUDEDIR and BOOST_LIBRARYDIR to help find Boost.")
