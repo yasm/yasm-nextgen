8cdd226bfa29d8761d104c0e3c3d0fafff35569a
Peter Johnson <peter@tortall.net>
2009-06-18 00:56:48 -0700

    Revert "Debug: Remove CommandLine dependency for now."
    This reverts commit 19669acf31a60123d011c307f547b4511e6fb584.

--- lib/Support/Debug.cpp
+++ lib/Support/Debug.cpp
@@ -23,17 +23,45 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include "llvm/Support/CommandLine.h"
 #include "llvm/Support/Debug.h"
 using namespace llvm;
 
 bool llvm::DebugFlag;  // DebugFlag - Exported boolean set by the -debug option
+
+namespace {
+#ifndef NDEBUG
+  // -debug - Command line option to enable the DEBUG statements in the passes.
+  // This flag may only be enabled in debug builds.
+  static cl::opt<bool, true>
+  Debug("debug", cl::desc("Enable debug output"), cl::Hidden,
+        cl::location(DebugFlag));
+
+  static std::string CurrentDebugType;
+  static struct DebugOnlyOpt {
+    void operator=(const std::string &Val) const {
+      DebugFlag |= !Val.empty();
+      CurrentDebugType = Val;
+    }
+  } DebugOnlyOptLoc;
+
+  static cl::opt<DebugOnlyOpt, true, cl::parser<std::string> >
+  DebugOnly("debug-only", cl::desc("Enable a specific type of debug output"),
+            cl::Hidden, cl::value_desc("debug string"),
+            cl::location(DebugOnlyOptLoc), cl::ValueRequired);
+#endif
+}
 
 // isCurrentDebugType - Return true if the specified string is the debug type
 // specified on the command line, or if none was specified on the command line
 // with the -debug-only=X option.
 //
 bool llvm::isCurrentDebugType(const char *DebugType) {
+#ifndef NDEBUG
+  return CurrentDebugType.empty() || DebugType == CurrentDebugType;
+#else
   return false;
+#endif
 }
 
 // getErrorOutputStream - Returns the error output stream (std::cerr). This
