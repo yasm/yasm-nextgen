675a08eb164299f960232a5578a0f8ef9171a058
Peter Johnson <peter@tortall.net>
2009-06-20 15:40:04 -0700

    AssocData: Use YASM_LIB_EXPORT in friend declaration.
    Visual Studio wants this to match actual declaration.

--- include/yasmx/AssocData.h
+++ include/yasmx/AssocData.h
@@ -66,8 +66,9 @@ operator<< (marg_ostream& os, const AssocData& data)
 /// Associated data container.
 class YASM_LIB_EXPORT AssocDataContainer
 {
-    friend marg_ostream& operator<< (marg_ostream& os,
-                                     const AssocDataContainer& container);
+    friend YASM_LIB_EXPORT
+	marg_ostream& operator<< (marg_ostream& os,
+                              const AssocDataContainer& container);
 
     typedef std::map<const void*, AssocData*> AssocMap;
     AssocMap m_assoc_map;
