ea721258cf22e6ba8b32b244fdeb6ef9de0cd6c1
Peter Johnson <peter@tortall.net>
2012-04-27 22:39:02 -0700

    gas parser: In directives, look up special symbols even if not '@' prefixed.
    This allows e.g. object formats to have named section flags and attributes
    without polluting the object's symbol table.

--- modules/parsers/gas/GasParser_parse.cpp
+++ modules/parsers/gas/GasParser_parse.cpp
@@ -1596,6 +1596,42 @@ GasParser::ParseInsn()
     return Insn::Ptr(0);
 }
 
+// Directive expression term: only difference from normal is that we look
+// at unescaped (no @ sign) labels to see if they're special.
+namespace {
+class ParseDirectiveExprTerm : public ParseExprTerm
+{
+    Object& m_object;
+public:
+    ParseDirectiveExprTerm(Object& object) : m_object(object) {}
+    ~ParseDirectiveExprTerm() {}
+
+    bool operator() (Expr& e, ParserImpl& parser, bool* handled) const;
+};
+} // anonymous namespace
+
+bool
+ParseDirectiveExprTerm::operator()
+    (Expr& e, ParserImpl& parser, bool* handled) const
+{
+    if (parser.m_token.is(GasToken::identifier) ||
+        parser.m_token.is(GasToken::label))
+    {
+        IdentifierInfo* ii = parser.m_token.getIdentifierInfo();
+        SymbolRef sym = m_object.FindSpecialSymbol(ii->getName());
+        if (sym)
+        {
+            SourceLocation id_source = parser.ConsumeToken();
+            e = Expr(sym, id_source);
+            *handled = true;
+            return true;
+        }
+    }
+
+    *handled = false;
+    return true;
+}
+
 bool
 GasParser::ParseDirective(NameValues* nvs)
 {
@@ -1620,7 +1656,8 @@ GasParser::ParseDirective(NameValues* nvs)
                     {
                         SourceLocation e_src = m_token.getLocation();
                         Expr::Ptr e(new Expr);
-                        if (!ParseExpr(*e))
+                        ParseDirectiveExprTerm parse_term(*m_object);
+                        if (!ParseExpr(*e, &parse_term))
                             return false;
                         nvs->push_back(new NameValue(e));
                         nvs->back().setValueRange(
@@ -1716,7 +1753,8 @@ GasParser::ParseDirective(NameValues* nvs)
             {
                 SourceLocation e_src = m_token.getLocation();
                 Expr::Ptr e(new Expr);
-                if (!ParseExpr(*e))
+                ParseDirectiveExprTerm parse_term(*m_object);
+                if (!ParseExpr(*e, &parse_term))
                     return false;
                 nvs->push_back(new NameValue(e));
                 nvs->back().setValueRange(
